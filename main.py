080824c6d6868657d715cfb0f41121f1000f72ea
JTalking <josiahfabbott@gmail.com>
2025-05-23 06:54:29 -0400

    I: Test 1

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -13,8 +15,10 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.container = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
@@ -32,6 +36,11 @@ def main():
         updatable.update(dt)
 
         screen.fill("black")
+
+        for asteroid in asteroids:
+            if asteroid.collision_check(player):
+                print ("Game over!")
+                sys.exit()
 
         for obj in drawable:
             obj.draw(screen)
